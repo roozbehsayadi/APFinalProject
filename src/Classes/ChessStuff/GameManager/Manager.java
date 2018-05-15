21e0c11730e4c9054d94d5d056d9992bae651178
monjar <amirali.mnj@gmail.com>
2018-05-15 01:36:17 -0700

    Black player added!

--- src/Classes/ChessStuff/GameManager/Manager.java
+++ src/Classes/ChessStuff/GameManager/Manager.java
@@ -1,4 +1,5 @@
 package Classes.ChessStuff.GameManager;
 
 public class Manager {
+    private
 }
---
+++ src/Classes/ChessStuff/Player/BlackPlayer.java
@@ -0,0 +1,10 @@
+package Classes.ChessStuff.Player;
+
+import Classes.ChessStuff.SafHe.ChessBoard;
+
+public class BlackPlayer implements Player {
+    @Override
+    public ChessBoard makeMove(ChessBoard chessBoard) {
+        return null;
+    }
+}
